7ed7afeb478a5945198c5bfb0c09095fd262717f
Pravin Kanth <r.pravinkanth653@gmail.com>
2023-01-20 12:38:55 +0530

    Time: 47 ms (75.59%), Space: 14 MB (62.91%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -0,0 +1,10 @@
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        dp= [0] * n
+        ct=0
+        dp[0] = 1
+        for i in range(1, n):
+            dp[i] = ct + dp[i - delay] - dp[i - forget]
+            ct = dp[i]
+        return(sum(dp[n-forget:])%1000000007)
+        
