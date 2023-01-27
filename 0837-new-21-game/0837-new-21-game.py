e801e1e3723a198ecf11d5a5d521c71828427471
Pravin Kanth <r.pravinkanth653@gmail.com>
2023-01-27 22:57:05 +0530

    Time: 106 ms (54.14%), Space: 14.4 MB (38.85%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        ans=[0]*(k + maxPts)
+        sumans=0
+        for i in range(k + maxPts - 1, k - 1, -1):
+            if i <= n:
+                ans[i] = 1
+            sumans += ans[i]
+        for i in range(k - 1, -1, -1):
+            ans[i] = sumans / maxPts;
+            sumans += ans[i]
+            sumans -= ans[i + maxPts]
+        return ans[0]
